dba7fdb4190abecdbdb59560a9c3530d8fb836e2
prem2282 <prem2282@gmail.com>
2023-12-17 21:44:40 +0530

    images added

--- src/components/academics/ChapterCard.vue
+++ src/components/academics/ChapterCard.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <div class="button-container">
-      
+    <div class="subject-card-container">
       <div v-for="(item, index) in getChapterListForSubject" :key="index">
         <transition
           appear
@@ -9,14 +8,22 @@
           enter-active-class="animated fadeIn"
           leave-active-class="animated fadeOut"
         >
-          <q-btn class="chapter-button" @click="selectChapter(item)">
+          <div>
+            <q-card class="subject-card elevated" @click="selectChapter(item)">
+              <q-img :src="imageSource(item)"> </q-img>
+              <q-card-section class="text-center bg-purple-9 text-white text-bold">
+                {{ item.chapter }}
+              </q-card-section>
+            </q-card>
+          </div>
+
+          <!-- <q-btn class="chapter-button" @click="selectChapter(item)">
             <div class="button-content">
               <div class="chapter-text">{{ item.chapter }}</div>
             </div>
-          </q-btn>
+          </q-btn> -->
         </transition>
       </div>
-
     </div>
   </div>
 </template>
@@ -40,6 +47,9 @@ export default {
   methods: {
     selectChapter(item) {
       this.academicsStore.selectChapter(item);
+    },
+    imageSource(item) {
+      return `images/lessons/${item.quizId}.png`;
     },
   },
 };
